537e25cd6c4e66f39ac06a0b305d99588b416d6a
Mathieu1408 <mtran20@myges.fr>
2025-05-07 14:58:06 +0200

    Update db.client.js

--- db.client.js
+++ db.client.js
@@ -1,23 +1,20 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
-// database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  },
+  }
 );
-
-// authentication and synchronization
-sequelize.authenticate()
-  .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
-  })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
 
 module.exports = sequelize;
